c86468d55f7e22f6d92d0ef15244db560010146c
Anh Dung Bui <dungba.sg@gmail.com>
2019-05-03 10:57:33 +0700

    refactor

--- gridgo-example-gridgoboot/src/main/java/io/gridgo/example/gridgoboot/gateways/HttpServerGateway.java
+++ gridgo-example-gridgoboot/src/main/java/io/gridgo/example/gridgoboot/gateways/HttpServerGateway.java
@@ -3,7 +3,6 @@ package io.gridgo.example.gridgoboot.gateways;
 import static io.gridgo.example.gridgoboot.Constant.GATEWAY_HTTP_SERVER;
 import static io.gridgo.example.gridgoboot.Constant.GATEWAY_KAFKA_PRODUCER;
 
-import lombok.Setter;
 import io.gridgo.bean.BObject;
 import io.gridgo.boot.support.annotations.Connector;
 import io.gridgo.boot.support.annotations.Gateway;
@@ -13,7 +12,7 @@ import io.gridgo.core.GridgoContext;
 import io.gridgo.core.impl.AbstractProcessor;
 import io.gridgo.core.support.RoutingContext;
 import io.gridgo.framework.support.Message;
-import io.gridgo.framework.support.Payload;
+import lombok.Setter;
 
 /**
  * Created by nauh94@gmail.com on 2019-04-12
@@ -31,9 +30,8 @@ public class HttpServerGateway extends AbstractProcessor {
         var msg = rc.getMessage();
         var deferred = rc.getDeferred();
         // push a received message to kafka
-        producer.sendWithAck(Message.of(Payload.of(msg.getPayload().getBody())))
-                .done(message -> deferred.resolve(
-                        Message.ofAny(BObject.of("message", "we has received your order!"))))
-                .fail(deferred::reject);
+        producer.sendAnyWithAck(msg.body()) //
+                .map(message -> Message.ofAny(BObject.of("message", "we has received your order!"))) //
+                .forward(deferred);
     }
 }
